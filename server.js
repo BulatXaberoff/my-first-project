679973cbf0464439439aacb3229ef9f4895c027c
Xabears <80531785+BulatXaberoff@users.noreply.github.com>
2024-03-14 20:08:21 +0300

    added docker

--- server.js
+++ server.js
@@ -14,3 +14,18 @@ const server = http.createServer((req, res) => {
 });
 //ver running at http://localhost:3000/");
 //});
+
+
+express = require('express');
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
